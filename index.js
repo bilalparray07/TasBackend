21f3b73dd342afef28a3bd8dd6c18d13307eba60
Bilal Ahmad <dev.renosoftwares@gmail.com>
2024-03-30 15:26:40 +0530

    added cors

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const router = require("./src/routes"); // Corrected import path
-
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI =
   "mongodb+srv://sorieasal:sorieasal@nodejs.3k8ji4v.mongodb.net/";
 
 mongoose.connect(MONGODB_URI);
-
+app.use(cors());
 app.use(bodyParser.json());
 // Serve static files from the public directory
 app.use(express.static("src/public"));
